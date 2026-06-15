512b476cac14d45fe96d1796d2c2e1d51e973952
Freddie Nash <Freddie.Nash@thinksmartbox.com>
2026-06-15 16:57:29 +0100

    Bleh

--- samples/Sandbox/Program.cs
+++ samples/Sandbox/Program.cs
@@ -11,8 +11,8 @@ namespace Sandbox
         public static AppBuilder BuildAvaloniaApp() =>
             AppBuilder.Configure<App>()
                 .UsePlatformDetect()
-                .With(new Win32PlatformOptions() { RenderingMode = [Win32RenderingMode.Software] }) // software rendering is less sensitive to system configuration
-                .With(new SkiaOptions() { MaxGpuResourceSizeBytes = 200_000_000 }) // lots of resources is necessary for adequate performance with large images if not using Software rendering
+                .With(new Win32PlatformOptions() { RenderingMode = [Win32RenderingMode.AngleEgl] }) // software rendering is less sensitive to system configuration
+                .With(new SkiaOptions() { MaxGpuResourceSizeBytes = 100_000_000 }) // lots of resources is necessary for adequate performance with large images if not using Software rendering
                 .With(new CompositionOptions() { UseRegionDirtyRectClipping = true }) // makes more consistent behaviour when throwing the mouse around the screen
                 .LogToTrace();
     }
